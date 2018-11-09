b764ce7ec5e2b68d3fc4d95ae79cb9792ad87253
Alex <aanderson65@wisc.edu>
2018-11-09 09:29:26 -0600

    NestedLoop

--- src/NestedLoop.java
+++ src/NestedLoop.java
@@ -1,12 +1,40 @@
 /**
- * Suggestion: Printing triangles or patterns
+ * Nested Loops
+ * 
+ * Nested loops allow us to efficiently apply operations on a nested
+ *    structure like 2 dimensional boards. The key is correctly initializing
+ *    the index variables and setting the conditions of the for loops
  * 
  * @author Alex
  */
 
+import java.util.Scanner;
+
 public class NestedLoop {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.print("Enter the triangle's height: ");
+		if(!scanner.hasNextInt())
+		{
+			// Don't accept bad input
+			System.out.println("Expected a number. Goodbye.");
+			return;
+		}
+		
+		int height = scanner.nextInt();
+		scanner.nextLine();
+		
+		for(int rowNum = 0; rowNum < height; rowNum++)	// For each row
+		{
+			// Note: the program stops once the number of columns exceeds the number of rows
+			for(int columnNum = 0; columnNum <= rowNum; columnNum++)  // For each column in the row
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
 	}
 }
