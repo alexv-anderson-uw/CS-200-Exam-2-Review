e51695169b36a1889e28e9add0f5bd9697ff0462
Alex <aanderson65@wisc.edu>
2018-11-09 09:45:58 -0600

    CM Args

--- src/CMArguments.java
+++ src/CMArguments.java
@@ -1,12 +1,28 @@
 /**
- * Suggestion: Access and print command-line arguments
+ * Command Line Arguments
+ * 
+ * Command line arguments are given to the program at the moment it
+ *     is executed. The arguments come from the JVM which gets them
+ *     from the command used to start the program.
  * 
  * @author Alex
  */
+
+import java.util.Arrays;
 
 public class CMArguments {
 
 	public static void main(String[] args) {
 		
+		//Note: the args parameter is empty if no parameters are given
+		if(args.length == 0)
+		{
+			System.out.println("No command line arguments were passed");
+		}
+		else
+		{
+			System.out.println(args.length + " command line arguments were passed");
+			System.out.println(Arrays.toString(args));
+		}
 	}
 }
